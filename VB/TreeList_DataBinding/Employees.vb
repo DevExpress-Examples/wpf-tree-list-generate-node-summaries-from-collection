2483539e1d0e394e81ebf456f1e38f13f0c8990f
DevExpressExampleBot <fakeEmail>
2023-04-14 17:29:42 +0400

    Source auto update [skip ci]

--- VB/TreeList_DataBinding/Employees.vb
+++ VB/TreeList_DataBinding/Employees.vb
@@ -1,52 +1,47 @@
-﻿Imports DevExpress.Mvvm
-Imports System
-Imports System.Collections.Generic
+Imports DevExpress.Mvvm
 Imports System.Collections.ObjectModel
 
 Namespace TreeList_DataBinding
 
-	Public Class Employee
-		Inherits BindableBase
+    Public Class Employee
+        Inherits BindableBase
 
-		Public Property ID() As Integer
-		Public Property ParentID() As Integer
-		Public Property Name() As String
-		Public Property Statistics() As Double
-		Public Property Position() As String
-		Public Property Department() As String
-	End Class
+        Public Property ID As Integer
 
-	Public NotInheritable Class Stuff
+        Public Property ParentID As Integer
 
-		Private Sub New()
-		End Sub
+        Public Property Name As String
 
-		Public Shared Function GetStuff() As ObservableCollection(Of Employee)
-'INSTANT VB NOTE: The variable stuff was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
-			Dim stuff_Renamed As New ObservableCollection(Of Employee)()
-			stuff_Renamed.Add(New Employee() With {.ID = 1, .ParentID = 0, .Name = "Gregory S. Price", .Statistics = 2350, .Department = "", .Position = "President"})
-			stuff_Renamed.Add(New Employee() With {.ID = 2, .ParentID = 1, .Name = "Irma R. Marshall", .Statistics = 2150, .Department = "Marketing", .Position = "Vice President"})
-			stuff_Renamed.Add(New Employee() With {.ID = 3, .ParentID = 1, .Name = "John C. Powell", .Statistics = 3350, .Department = "Operations", .Position = "Vice President"})
-			stuff_Renamed.Add(New Employee() With {.ID = 4, .ParentID = 1, .Name = "Christian P. Laclair", .Statistics = 2350, .Department = "Production", .Position = "Vice President"})
-			stuff_Renamed.Add(New Employee() With {.ID = 5, .ParentID = 1, .Name = "Karen J. Kelly", .Statistics = 1350, .Department = "Finance", .Position = "Vice President"})
+        Public Property Statistics As Double
 
-			stuff_Renamed.Add(New Employee() With {.ID = 6, .ParentID = 2, .Name = "Brian C. Cowling", .Statistics = 1650, .Department = "Marketing", .Position = "Manager"})
-			stuff_Renamed.Add(New Employee() With {.ID = 7, .ParentID = 2, .Name = "Thomas C. Dawson", .Statistics = 1050, .Department = "Marketing", .Position = "Manager"})
-			stuff_Renamed.Add(New Employee() With {.ID = 8, .ParentID = 2, .Name = "Angel M. Wilson", .Statistics = 2500, .Department = "Marketing", .Position = "Manager"})
-			stuff_Renamed.Add(New Employee() With {.ID = 9, .ParentID = 2, .Name = "Bryan R. Henderson", .Statistics = 250, .Department = "Marketing", .Position = "Manager"})
+        Public Property Position As String
 
-			stuff_Renamed.Add(New Employee() With {.ID = 10, .ParentID = 3, .Name = "Harold S. Brandes", .Statistics = 350, .Department = "Operations", .Position = "Manager"})
-			stuff_Renamed.Add(New Employee() With {.ID = 11, .ParentID = 3, .Name = "Michael S. Blevins", .Statistics = 3550, .Department = "Operations", .Position = "Manager"})
-			stuff_Renamed.Add(New Employee() With {.ID = 12, .ParentID = 3, .Name = "Jan K. Sisk", .Statistics = 1750, .Department = "Operations", .Position = "Manager"})
-			stuff_Renamed.Add(New Employee() With {.ID = 13, .ParentID = 3, .Name = "Sidney L. Holder", .Statistics = 1900, .Department = "Operations", .Position = "Manager"})
+        Public Property Department As String
+    End Class
 
-			stuff_Renamed.Add(New Employee() With {.ID = 14, .ParentID = 4, .Name = "James L. Kelsey", .Statistics = 5010, .Department = "Production", .Position = "Manager"})
-			stuff_Renamed.Add(New Employee() With {.ID = 15, .ParentID = 4, .Name = "Howard M. Carpenter", .Statistics = 3400, .Department = "Production", .Position = "Manager"})
-			stuff_Renamed.Add(New Employee() With {.ID = 16, .ParentID = 4, .Name = "Jennifer T. Tapia", .Statistics = 2350, .Department = "Production", .Position = "Manager"})
+    Public Module Staff
 
-			stuff_Renamed.Add(New Employee() With {.ID = 17, .ParentID = 5, .Name = "Judith P. Underhill", .Statistics = 2950, .Department = "Finance", .Position = "Manager"})
-			stuff_Renamed.Add(New Employee() With {.ID = 18, .ParentID = 5, .Name = "Russell E. Belton", .Statistics = 2150, .Department = "Finance", .Position = "Manager"})
-			Return stuff_Renamed
-		End Function
-	End Class
+        Public Function GetStaff() As ObservableCollection(Of Employee)
+            Dim employees As ObservableCollection(Of Employee) = New ObservableCollection(Of Employee)()
+            employees.Add(New Employee() With {.ID = 1, .ParentID = 0, .Name = "Gregory S. Price", .Statistics = 2350, .Department = "", .Position = "President"})
+            employees.Add(New Employee() With {.ID = 2, .ParentID = 1, .Name = "Irma R. Marshall", .Statistics = 2150, .Department = "Marketing", .Position = "Vice President"})
+            employees.Add(New Employee() With {.ID = 3, .ParentID = 1, .Name = "John C. Powell", .Statistics = 3350, .Department = "Operations", .Position = "Vice President"})
+            employees.Add(New Employee() With {.ID = 4, .ParentID = 1, .Name = "Christian P. Laclair", .Statistics = 2350, .Department = "Production", .Position = "Vice President"})
+            employees.Add(New Employee() With {.ID = 5, .ParentID = 1, .Name = "Karen J. Kelly", .Statistics = 1350, .Department = "Finance", .Position = "Vice President"})
+            employees.Add(New Employee() With {.ID = 6, .ParentID = 2, .Name = "Brian C. Cowling", .Statistics = 1650, .Department = "Marketing", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 7, .ParentID = 2, .Name = "Thomas C. Dawson", .Statistics = 1050, .Department = "Marketing", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 8, .ParentID = 2, .Name = "Angel M. Wilson", .Statistics = 2500, .Department = "Marketing", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 9, .ParentID = 2, .Name = "Bryan R. Henderson", .Statistics = 250, .Department = "Marketing", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 10, .ParentID = 3, .Name = "Harold S. Brandes", .Statistics = 350, .Department = "Operations", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 11, .ParentID = 3, .Name = "Michael S. Blevins", .Statistics = 3550, .Department = "Operations", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 12, .ParentID = 3, .Name = "Jan K. Sisk", .Statistics = 1750, .Department = "Operations", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 13, .ParentID = 3, .Name = "Sidney L. Holder", .Statistics = 1900, .Department = "Operations", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 14, .ParentID = 4, .Name = "James L. Kelsey", .Statistics = 5010, .Department = "Production", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 15, .ParentID = 4, .Name = "Howard M. Carpenter", .Statistics = 3400, .Department = "Production", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 16, .ParentID = 4, .Name = "Jennifer T. Tapia", .Statistics = 2350, .Department = "Production", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 17, .ParentID = 5, .Name = "Judith P. Underhill", .Statistics = 2950, .Department = "Finance", .Position = "Manager"})
+            employees.Add(New Employee() With {.ID = 18, .ParentID = 5, .Name = "Russell E. Belton", .Statistics = 2150, .Department = "Finance", .Position = "Manager"})
+            Return employees
+        End Function
+    End Module
 End Namespace
--- VB/TreeList_DataBinding/MainWindow.xaml.vb
+++ VB/TreeList_DataBinding/MainWindow.xaml.vb
@@ -1,59 +1,55 @@
-﻿Imports DevExpress.Data
+Imports DevExpress.Data
 Imports DevExpress.Mvvm
-Imports DevExpress.Xpf.Grid
-Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 Imports System.Windows
 
 Namespace TreeList_DataBinding
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
+    Public Partial Class MainWindow
+        Inherits Window
 
-			InitializeComponent()
-			treeListView1.ExpandAllNodes()
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.treeListView1.ExpandAllNodes()
+        End Sub
+    End Class
 
-	Public Class ViewModel
-		Inherits ViewModelBase
+    Public Class ViewModel
+        Inherits ViewModelBase
 
-		Public Property SummaryList() As ObservableCollection(Of SummaryTask)
-			Get
-				Return GetProperty(Function() SummaryList)
-			End Get
-			Set(ByVal value As ObservableCollection(Of SummaryTask))
-				SetProperty(Function() SummaryList, value)
-			End Set
-		End Property
+        Public Property SummaryList As ObservableCollection(Of SummaryTask)
+            Get
+                Return GetProperty(Function() Me.SummaryList)
+            End Get
 
-		Public Property SourceList() As ObservableCollection(Of Employee)
-			Get
-				Return GetProperty(Function() SourceList)
-			End Get
-			Set(ByVal value As ObservableCollection(Of Employee))
-				SetProperty(Function() SourceList, value)
-			End Set
-		End Property
+            Set(ByVal value As ObservableCollection(Of SummaryTask))
+                SetProperty(Function() SummaryList, value)
+            End Set
+        End Property
 
+        Public Property SourceList As ObservableCollection(Of Employee)
+            Get
+                Return GetProperty(Function() Me.SourceList)
+            End Get
 
+            Set(ByVal value As ObservableCollection(Of Employee))
+                SetProperty(Function() SourceList, value)
+            End Set
+        End Property
 
-		Public Sub New()
-			SourceList = Stuff.GetStuff()
-			SummaryList = New ObservableCollection(Of SummaryTask)()
+        Public Sub New()
+            SourceList = GetStaff()
+            SummaryList = New ObservableCollection(Of SummaryTask)()
+            SummaryList.Add(New SummaryTask() With {.FieldName = "Statistics", .Type = SummaryItemType.Max})
+            SummaryList.Add(New SummaryTask() With {.FieldName = "Statistics", .Type = SummaryItemType.Average})
+            SummaryList.Add(New SummaryTask() With {.FieldName = "Department", .Type = SummaryItemType.Count})
+        End Sub
+    End Class
 
-			SummaryList.Add(New SummaryTask() With {.FieldName = "Statistics", .Type = SummaryItemType.Max})
-			SummaryList.Add(New SummaryTask() With {.FieldName = "Statistics", .Type = SummaryItemType.Average})
-			SummaryList.Add(New SummaryTask() With {.FieldName = "Department", .Type = SummaryItemType.Count})
-		End Sub
-	End Class
+    Public Class SummaryTask
 
-	Public Class SummaryTask
-		Public Property FieldName() As String
-		Public Property Type() As SummaryItemType
-	End Class
+        Public Property FieldName As String
+
+        Public Property Type As SummaryItemType
+    End Class
 End Namespace
